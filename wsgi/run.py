ea79e3358698e3e89ccf50782a65c3b5863869db
Abhiram Ravikumar <abhi12ravi@gmail.com>
2015-10-12 22:21:17 +0530

    Changing author name

--- wsgi/run.py
+++ wsgi/run.py
@@ -10,16 +10,16 @@ app = Flask(__name__)
 @app.route("/index", methods=["GET", "POST"])
 def index():
 	notes = [{ "name":"First Note Ever",
-            "author":"Angel",
+            "author":"Abhiram",
             "content":"This text is coming from the content field"
            },
        { "name":"Finish this Blog",
-            "author":"Angel",
+            "author":"Abhiram",
             "content":"Show the template control structures"
 
        },
        {"name":"Deploy this app to OpenShift",
-            "author":"Angel",
+            "author":"Abhiram",
             "content":"When finished coding this app deploy it to OpenShift"
 
        }]
